6481316634cbc6fc5360291cd890a185220bb205
Spoiled Unknown <shanjusamsong254@gmail.com>
2023-10-30 15:05:23 +0530

    minor improvements

---
+++ Assets/NDS/First Person Controller/Scripts/Gameplay Scripts/Wall Hit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using NDS.UniversalWeaponSystem.ShootableObjects;
+
+public class WallHit : ShootableObject
+{
+    public GameObject particlesPrefab;
+
+    public override void OnHit(RaycastHit hit)
+    {
+        GameObject instantiatedParticles = Instantiate(particlesPrefab, hit.point + hit.normal * 0.05f, Quaternion.LookRotation(hit.normal), transform.root.parent);
+
+        Destroy(instantiatedParticles, 2f);
+    }
+}
